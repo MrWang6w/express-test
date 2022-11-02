a232d03ec78ef69daf4222b83279deee88136e48
王亮 <wangliang15@shein.com>
2022-11-02 16:58:41 +0800

    HTTP 处理程序

--- api/product.js
+++ api/product.js
@@ -1,27 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 let count = 1;
 
-router.get('/count',(req,res)=>{
+
+router.get('/count', (req, res) => {
   count += 1;
   res.json({
-    code:0,
-    count
-  })
-})
+    code: 0,
+    count,
+  });
+});
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     res.json({
       status: 200,
-      message: "Get data has successfully",
+      message: 'Get data has successfully',
     });
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Server error");
+    return res.status(500).send('Server error');
   }
 });
-
-
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
+const { serve } = require('inngest/express');
+const inngestFunction = require('./api/inngestFunction');
 
 app.use(express.json({ extended: false }));
 
+app.use('/api/inngest', serve('My App', [inngestFunction]));
 app.use("/", product);
 
 const PORT = process.env.PORT || 8080;
