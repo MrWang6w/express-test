dd527aa728f7f6cc5b979572ee58058b81517d36
王亮 <wangliang15@shein.com>
2022-11-02 16:28:59 +0800

    test

---
+++ api/product.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+let count = 1;
+
+router.get('/count',(req,res)=>{
+  count += 1;
+  res.json({
+    code:0,
+    count
+  })
+})
+
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
