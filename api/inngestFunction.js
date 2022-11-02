925955e17eface9d3ee79d5044fb342eff1a6c30
王亮 <wangliang15@shein.com>
2022-11-02 17:25:01 +0800

    尝试写入循环执行异步函数 达到多线程

--- api/inngestFunction.js
+++ api/inngestFunction.js
@@ -2,14 +2,22 @@ const { createScheduledFunction } = require('inngest');
 const axios = require('axios');
 
 const job = async () => {
-  const { data } = await axios('https://express-test-chi.vercel.app/count');
-  console.log('data:', data);
-  return data; 
+  for (let i = 0; i < 100; i++) {
+  await  new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true)
+      }, 2500);
+    })
+    const { data } = await axios('https://express-test-chi.vercel.app/count');
+    console.log('data:', data);
+  }
+
+  return data;
 };
 
 
 module.exports = createScheduledFunction(
   'cron request',
-  '*/1 * * * *', 
-  job 
+  '*/10 * * * *',
+  job
 )
